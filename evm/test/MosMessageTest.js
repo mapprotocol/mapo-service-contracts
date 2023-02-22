75ef40d2bf38d812f859e6a7e92927961692498c
Phil <phil@maplabs.io>
2023-02-23 03:40:10 +0800

    Tidy test codes.

--- evm/test/MosMessageTest.js
+++ evm/test/MosMessageTest.js
@@ -1,23 +1,18 @@
 const {ethers} = require("hardhat");
 const {expect} = require("chai");
 
-describe("MAPOmnichainServiceV3 start test", () =>{
+describe("MAPO ServiceV3 start test", () =>{
     let owner;
     let addr1;
 
-    let MosMessage;
-    let mosMessage;
+    let mos;
 
-    let LightNode;
     let lightNode;
 
-    let Wrapped;
     let wrapped;
 
-    let PingPong;
     let pingPong;
 
-    let MessageFee;
     let messageFee;
 
     beforeEach(async () =>{
@@ -26,44 +21,44 @@ describe("MAPOmnichainServiceV3 start test", () =>{
 
     it('contract deploy init', async function () {
 
-        MosMessage = await ethers.getContractFactory("MapoServiceV3");
-        mosMessage = await MosMessage.deploy();
-        console.log("mosMessage address:",mosMessage.address);
+        let mosContract = await ethers.getContractFactory("MapoServiceV3");
+        mos = await mosContract.deploy();
+        console.log("mosMessage address:",mos.address);
 
-        Wrapped = await ethers.getContractFactory("Wrapped");
-        wrapped = await Wrapped.deploy();
+        let wrappedContract = await ethers.getContractFactory("Wrapped");
+        wrapped = await wrappedContract.deploy();
         console.log("Wrapped:",wrapped.address);
 
-        LightNode = await ethers.getContractFactory("LightNode");
-        lightNode = await  LightNode.deploy();
+        let lightNodeContract = await ethers.getContractFactory("LightNode");
+        lightNode = await  lightNodeContract.deploy();
 
-        PingPong = await ethers.getContractFactory("PingPong");
-        pingPong = await  PingPong.deploy();
+        let pingPongContract = await ethers.getContractFactory("PingPong");
+        pingPong = await  pingPongContract.deploy();
 
-        let data  = await mosMessage.initialize(wrapped.address,lightNode.address);
+        let data  = await mos.initialize(wrapped.address, lightNode.address);
 
-        const MapCrossChainServiceProxy = await ethers.getContractFactory("MapoServiceProxyV3");
-        let mossp = await MapCrossChainServiceProxy.deploy(mosMessage.address,data.data);
-        await mossp.deployed()
-        mosMessage = MosMessage.attach(mossp.address);
+        let proxyContract = await ethers.getContractFactory("MapoServiceProxyV3");
+        let proxy = await proxyContract.deploy(mos.address, data.data);
+        await proxy.deployed()
+        mos = mosContract.attach(proxy.address);
 
-        MessageFee = await ethers.getContractFactory("MessageFee");
-        messageFee = await  MessageFee.deploy();
+        let feeContract = await ethers.getContractFactory("MessageFee");
+        messageFee = await  feeContract.deploy();
 
     });
 
     it('mosMessage set ', async function () {
-         await mosMessage.setRelayContract(5,"0x5FC8d32690cc91D4c39d9d3abcBD16989F875707");
+         await mos.setRelayContract(5,"0x5FC8d32690cc91D4c39d9d3abcBD16989F875707");
 
-         await mosMessage.addWhiteList(pingPong.address,true);
+         await mos.addWhiteList(pingPong.address,true);
 
-         await mosMessage.addWhiteList(owner.address,true);
+         await mos.addWhiteList(owner.address,true);
 
-         await mosMessage.registerChain(pingPong.address,97,"true");
+         await mos.registerChain(pingPong.address,97,"true");
 
-         await mosMessage.setMessageFee(messageFee.address);
+         await mos.setMessageFee(messageFee.address);
 
-        await pingPong.setWhiteList(mosMessage.address);
+        await pingPong.setWhiteList(mos.address);
 
         await messageFee.setMessageFee(97,pingPong.address,100);
 
@@ -73,20 +68,20 @@ describe("MAPOmnichainServiceV3 start test", () =>{
 
         let data = await pingPong.getData("hello","hello world");
 
-        await mosMessage.transferOut("97",[pingPong.address,data,"5000000","0"]);
+        await mos.transferOut("97",[pingPong.address,data,"5000000","0"]);
 
-        await expect(mosMessage.transferOut("212",[pingPong.address,data,"5000000","0"])).to.be.revertedWith("token not registered");
+        await expect(mos.transferOut("212",[pingPong.address,data,"5000000","0"])).to.be.revertedWith("token not registered");
 
-        await mosMessage.registerChain(pingPong.address,212,"true");
+        await mos.registerChain(pingPong.address,212,"true");
 
-        await expect(mosMessage.transferOut("212",[pingPong.address,data,"5000000","0"])).to.be.revertedWith("Only other chain");
+        await expect(mos.transferOut("212",[pingPong.address,data,"5000000","0"])).to.be.revertedWith("Only other chain");
 
-        await expect(mosMessage.connect(addr1).transferOut("97",[pingPong.address,data,"5000000","0"])).to.be.revertedWith("Non-whitelisted address");
+        await expect(mos.connect(addr1).transferOut("97",[pingPong.address,data,"5000000","0"])).to.be.revertedWith("Non-whitelisted address");
 
-        await mosMessage.registerChain(mosMessage.address,97,"true");
-        await expect(mosMessage.transferOut("97",[mosMessage.address,data,"5000000","0"])).to.be.revertedWith("Address has no message fee");
+        await mos.registerChain(mos.address,97,"true");
+        await expect(mos.transferOut("97",[mos.address,data,"5000000","0"])).to.be.revertedWith("Address has no message fee");
 
-        await expect(mosMessage.transferOut("97",[pingPong.address,data,"5000000","10"])).to.be.revertedWith("Not supported at present value");
+        await expect(mos.transferOut("97",[pingPong.address,data,"5000000","10"])).to.be.revertedWith("Not supported at present value");
 
 
     });
@@ -97,7 +92,7 @@ describe("MAPOmnichainServiceV3 start test", () =>{
 
         let receiptProof = "0xf902c0f902bd945fc8d32690cc91d4c39d9d3abcbd16989f875707f863a056877b1dbedc6754c111b951146b820fe6b723af0213fc415d44b05e1758dd85a00000000000000000000000000000000000000000000000000000000000000005a000000000000000000000000000000000000000000000000000000000000000d4b90240fa0695e96a5e7edd4f61def03d19534773886eca12cc281c2170c0078210b9f0000000000000000000000000000000000000000000000000000000000000004000000000000000000000000000000000000000000000000000000000000001e00000000000000000000000000000000000000000000000000000000000000020000000000000000000000000000000000000000000000000000000000000008000000000000000000000000000000000000000000000000000000000000000c000000000000000000000000000000000000000000000000000000000004c4b4000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000014cf7ed3acca5a467e9e704c703e8d87f634fb0fc900000000000000000000000000000000000000000000000000000000000000000000000000000000000000c4dd1d382400000000000000000000000000000000000000000000000000000000000000400000000000000000000000000000000000000000000000000000000000000080000000000000000000000000000000000000000000000000000000000000000568656c6c6f000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000b68656c6c6f20776f726c6400000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000"
 
-        await mosMessage.transferIn(5,receiptProof);
+        await mos.transferIn(5,receiptProof);
 
         expect(await pingPong.PingPongList("hello")).to.equal("hello world");
     });
--- evm/test/MosRelayMessageTest.js
+++ evm/test/MosRelayMessageTest.js
@@ -16,23 +16,14 @@ function getLogs(logData){
 
 
 
-describe("MAPOmnichainServiceRelayV3 start test", () =>{
+describe("MAPO ServiceRelayV3 start test", () =>{
     let owner;
     let addr1;
 
-    let MosMessageRelay;
-    let mosMessageRelay;
-
-    let LightNode;
+    let relay;
     let lightNode;
-
-    let Wrapped;
     let wrapped;
-
-    let PingPong;
     let pingPong;
-
-    let MessageFee;
     let messageFee;
 
     beforeEach(async () =>{
@@ -41,43 +32,43 @@ describe("MAPOmnichainServiceRelayV3 start test", () =>{
 
     it('constract deploy init', async function () {
 
-        MosMessageRelay = await ethers.getContractFactory("MapoServiceRelayV3");
-        mosMessageRelay = await MosMessageRelay.deploy();
-        console.log("mosMessageRelay address:",mosMessageRelay.address);
+        let relayContract = await ethers.getContractFactory("MapoServiceRelayV3");
+        relay = await relayContract.deploy();
+        console.log("mosMessageRelay address:",relay.address);
 
-        Wrapped = await ethers.getContractFactory("Wrapped");
-        wrapped = await Wrapped.deploy();
+        let wrappedContract = await ethers.getContractFactory("Wrapped");
+        wrapped = await wrappedContract.deploy();
         console.log("Wrapped:",wrapped.address);
 
-        LightNode = await ethers.getContractFactory("LightClientManager");
-        lightNode = await  LightNode.deploy();
+        let lightNodeContract = await ethers.getContractFactory("LightClientManager");
+        lightNode = await  lightNodeContract.deploy();
 
-        PingPong = await ethers.getContractFactory("PingPong");
-        pingPong = await  PingPong.deploy();
+        let pingPongContract = await ethers.getContractFactory("PingPong");
+        pingPong = await  pingPongContract.deploy();
 
-        let data  = await mosMessageRelay.initialize(wrapped.address,lightNode.address);
+        let data  = await relay.initialize(wrapped.address,lightNode.address);
 
-        const MapCrossChainServiceProxy = await ethers.getContractFactory("MapoServiceProxyV3");
-        let mossp = await MapCrossChainServiceProxy.deploy(mosMessageRelay.address,data.data);
-        await mossp.deployed()
-        mosMessageRelay = MosMessageRelay.attach(mossp.address);
+        let proxyContract = await ethers.getContractFactory("MapoServiceProxyV3");
+        let proxy = await proxyContract.deploy(relay.address,data.data);
+        await proxy.deployed()
+        relay = relayContract.attach(proxy.address);
 
-        MessageFee = await ethers.getContractFactory("MessageFee");
-        messageFee = await  MessageFee.deploy();
+        let feeContract = await ethers.getContractFactory("MessageFee");
+        messageFee = await  feeContract.deploy();
 
     });
 
     it('mosMessage set ', async function () {
-        await mosMessageRelay.registerChain(5,"0x5FC8d32690cc91D4c39d9d3abcBD16989F875707","1");
+        await relay.registerChain(5,"0x5FC8d32690cc91D4c39d9d3abcBD16989F875707","1");
 
-        await mosMessageRelay.addWhiteList(pingPong.address,true);
+        await relay.addWhiteList(pingPong.address,true);
 
-        await mosMessageRelay.addWhiteList(owner.address,true);
+        await relay.addWhiteList(owner.address,true);
 
 
-        await mosMessageRelay.setMessageFee(messageFee.address);
+        await relay.setMessageFee(messageFee.address);
 
-        await pingPong.setWhiteList(mosMessageRelay.address);
+        await pingPong.setWhiteList(relay.address);
 
         await messageFee.setMessageFee(97,pingPong.address,100);
 
@@ -89,9 +80,9 @@ describe("MAPOmnichainServiceRelayV3 start test", () =>{
 
         console.log(pingPong.address)
 
-        await mosMessageRelay.transferOut("97",[pingPong.address,data,"5000000","0"]);
+        await relay.transferOut("97",[pingPong.address,data,"5000000","0"]);
 
-        await expect(mosMessageRelay.transferOut("212",[pingPong.address,data,"5000000","0"])).to.be.revertedWith("Only other chain");
+        await expect(relay.transferOut("212",[pingPong.address,data,"5000000","0"])).to.be.revertedWith("Only other chain");
 
 
     });
@@ -102,14 +93,14 @@ describe("MAPOmnichainServiceRelayV3 start test", () =>{
 
         let receiptProof = "0xf902c0f902bd945fc8d32690cc91d4c39d9d3abcbd16989f875707f863a056877b1dbedc6754c111b951146b820fe6b723af0213fc415d44b05e1758dd85a00000000000000000000000000000000000000000000000000000000000000005a000000000000000000000000000000000000000000000000000000000000000d4b90240990d2a2ca04d3c48afd73f275a61e9fe79b16c87451862b65592d3a3860e52a6000000000000000000000000000000000000000000000000000000000000004000000000000000000000000000000000000000000000000000000000000001e00000000000000000000000000000000000000000000000000000000000000020000000000000000000000000000000000000000000000000000000000000008000000000000000000000000000000000000000000000000000000000000000c000000000000000000000000000000000000000000000000000000000004c4b40000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000144a679253410272dd5232b3ff7cf5dbb88f29531900000000000000000000000000000000000000000000000000000000000000000000000000000000000000c4dd1d382400000000000000000000000000000000000000000000000000000000000000400000000000000000000000000000000000000000000000000000000000000080000000000000000000000000000000000000000000000000000000000000000568656c6c6f000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000b68656c6c6f20776f726c6400000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000"
 
-        await mosMessageRelay.transferIn(5,receiptProof);
+        await relay.transferIn(5,receiptProof);
 
         expect(await pingPong.PingPongList("hello")).to.equal("hello world");
 
         //hello -> hello world bsc chain
         let receiptProof97 = "0xf902c0f902bd945fc8d32690cc91d4c39d9d3abcbd16989f875707f863a056877b1dbedc6754c111b951146b820fe6b723af0213fc415d44b05e1758dd85a000000000000000000000000000000000000000000000000000000000000000d4a00000000000000000000000000000000000000000000000000000000000000061b902406e624d148934cdef6962ac3cc94c52505c45ca0e526973de388e1fdbc159c145000000000000000000000000000000000000000000000000000000000000004000000000000000000000000000000000000000000000000000000000000001e00000000000000000000000000000000000000000000000000000000000000020000000000000000000000000000000000000000000000000000000000000008000000000000000000000000000000000000000000000000000000000000000c000000000000000000000000000000000000000000000000000000000004c4b40000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000144a679253410272dd5232b3ff7cf5dbb88f29531900000000000000000000000000000000000000000000000000000000000000000000000000000000000000c4dd1d382400000000000000000000000000000000000000000000000000000000000000400000000000000000000000000000000000000000000000000000000000000080000000000000000000000000000000000000000000000000000000000000000568656c6c6f000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000001568656c6c6f20776f726c642062736320636861696e000000000000000000000000000000000000000000000000000000000000000000000000000000"
 
-        await mosMessageRelay.transferIn(5,receiptProof97);
+        await relay.transferIn(5,receiptProof97);
 
         expect(await pingPong.PingPongList("hello")).to.equal("hello world");
 
